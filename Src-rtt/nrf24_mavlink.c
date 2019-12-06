381854ee347195b010856914841ff78f09a67814
SuWeipeng <weipeng.su@outlook.com>
2019-12-06 09:46:20 +0800

    测试 nRF24L01 收发 mavlink 报文的示例代码

--- Src-rtt/nrf24_mavlink.c
+++ Src-rtt/nrf24_mavlink.c
@@ -21,46 +21,46 @@ vel_target vel={.vel_x = 1.0f, .vel_y = 2.0f, .rad_z = 3.0f};
 
 void nrf24l01_mavlink_entry(void *param)
 {
-  struct hal_nrf24l01_port_cfg halcfg;
-  nrf24_cfg_t cfg;
-  int rlen;
-  static uint8_t tlen = 0;
-  uint8_t rbuf[32 + 1];
-  uint8_t tbuf[32] = "first\r\n";
-
-  _irq_init();
-
-  _nrf24_param_set(&cfg);
-  halcfg.ce_pin = NRF24L01_CE_PIN;
-  halcfg.spi_device_name = NRF24L01_SPI_DEVICE;
-  cfg.role = ROLE_PRX;    // PRX
-  cfg.ud = &halcfg;
-  cfg.use_irq = 1;        // True
-  nrf24_init(&cfg);
-
-  while (1) {
-    rlen = nrf24_irq_prx_run(rbuf, tbuf, tlen, _waitirq);
-    if (rlen > 0) {       // received data (also indicating that the previous frame of data was sent complete)
-      uint8_t i;
-      mavlink_message_t msg_receive;
-      mavlink_status_t mav_status;
-      for(i=0; i<32; i++) {
-        if(mavlink_parse_char(0, rbuf[i], &msg_receive, &mav_status)) {
-          switch (msg_receive.msgid) {
-          case MAVLINK_MSG_ID_SIMPLE: {
-            mavlink_simple_t packet;
-            mavlink_msg_simple_decode(&msg_receive, &packet);
-
-            mavlink_message_t msg_ack;
-            mavlink_msg_velocity_pack(0, 0, &msg_ack, vel.vel_x, vel.vel_y, vel.rad_z);
-            tlen = mavlink_msg_to_send_buffer((uint8_t *)tbuf, &msg_ack);
-            break;
-          }
-          }
-        }
-      }
-    }
-  }
+//  struct hal_nrf24l01_port_cfg halcfg;
+//  nrf24_cfg_t cfg;
+//  int rlen;
+//  static uint8_t tlen = 0;
+//  uint8_t rbuf[32 + 1];
+//  uint8_t tbuf[32] = "first\r\n";
+//
+//  _irq_init();
+//
+//  _nrf24_param_set(&cfg);
+//  halcfg.ce_pin = NRF24L01_CE_PIN;
+//  halcfg.spi_device_name = NRF24L01_SPI_DEVICE;
+//  cfg.role = ROLE_PRX;    // PRX
+//  cfg.ud = &halcfg;
+//  cfg.use_irq = 1;        // True
+//  nrf24_init(&cfg);
+//
+//  while (1) {
+//    rlen = nrf24_irq_prx_run(rbuf, tbuf, tlen, _waitirq);
+//    if (rlen > 0) {       // received data (also indicating that the previous frame of data was sent complete)
+//      uint8_t i;
+//      mavlink_message_t msg_receive;
+//      mavlink_status_t mav_status;
+//      for(i=0; i<32; i++) {
+//        if(mavlink_parse_char(0, rbuf[i], &msg_receive, &mav_status)) {
+//          switch (msg_receive.msgid) {
+//          case MAVLINK_MSG_ID_SIMPLE: {
+//            mavlink_simple_t packet;
+//            mavlink_msg_simple_decode(&msg_receive, &packet);
+//
+//            mavlink_message_t msg_ack;
+//            mavlink_msg_velocity_pack(0, 0, &msg_ack, vel.vel_x, vel.vel_y, vel.rad_z);
+//            tlen = mavlink_msg_to_send_buffer((uint8_t *)tbuf, &msg_ack);
+//            break;
+//          }
+//          }
+//        }
+//      }
+//    }
+//  }
 }
 
 static void _irq_cb(void *param)
--- packages/nrf24l01-latest/examples/sample_irq_prx.c
+++ packages/nrf24l01-latest/examples/sample_irq_prx.c
@@ -1,8 +1,10 @@
 #include <rtthread.h>
 #include "nrf24l01.h"
-
+#include "mavlink.h"
 #include "sample.h"
+#include "rtt_interface.h"
 
+extern vel_target vel;
 
 static rt_sem_t nrfirq_sem;
 
@@ -29,12 +31,24 @@ void sample_nrf24l01_task(void *param)
     nrf24_init(&cfg);
 
     while (1) {
-        rlen = nrf24_irq_prx_run(rbuf, tbuf, rt_strlen((char *)tbuf), _waitirq);
+        rlen = nrf24_irq_prx_run(rbuf, tbuf, 32, _waitirq);
         if (rlen > 0) {       // received data (also indicating that the previous frame of data was sent complete)
-            rbuf[rlen] = '\0';
-            rt_kputs((char *)rbuf);
-            rt_sprintf((char *)tbuf, "i-am-PRX:%dth\r\n", cnt);
-            cnt++;
+            uint8_t i;
+            mavlink_message_t msg_receive;
+            mavlink_status_t mav_status;
+            
+            for(i=0; i<32; i++) {
+              if(mavlink_parse_char(0, rbuf[i], &msg_receive, &mav_status)) {
+                switch (msg_receive.msgid) {
+                case MAVLINK_MSG_ID_SIMPLE: {
+                  mavlink_message_t msg_ack;
+                  mavlink_msg_velocity_pack(0, 0, &msg_ack, vel.vel_x, vel.vel_y, vel.rad_z);
+                  mavlink_msg_to_send_buffer((uint8_t *)tbuf, &msg_ack);
+                  break;
+                }
+                }
+              }
+            }
         }
         else {  // no data
             ;
@@ -64,7 +78,7 @@ static int nrf24l01_sample_init(void)
 {
     rt_thread_t thread;
 
-    thread = rt_thread_create("samNrfPRX", sample_nrf24l01_task, RT_NULL, 512, 3, 20);
+    thread = rt_thread_create("samNrfPRX", sample_nrf24l01_task, RT_NULL, 2048, 3, 20);
     rt_thread_startup(thread);
 
     return RT_EOK;
