56ab903ba94e87d04d32afe9c782d32925f6f101
SuWeipeng <weipeng.su@outlook.com>
2020-08-13 07:53:09 +0800

    与正点原子手柄适配 SPI 和 flexible-button

--- Inc/rtt_interface.h
+++ Inc/rtt_interface.h
@@ -5,24 +5,15 @@
 extern "C" {
 #endif
 
+#define AP_BUFFER_MAX_SIZE  (6)
 #include <entry.h>
 
-#define AP_BUFFER_MAX_SIZE  (6)
-#define LED_R_PIN           GET_PIN(C, 13)
-#define K1                  GET_PIN(B, 14)
-#define K2                  GET_PIN(B, 15)
-#define K3                  GET_PIN(B, 13)
-#define K4                  GET_PIN(B, 12)
-
-typedef union{
-  struct{
-    uint8_t key_value: 3;
-    uint8_t w_number : 2;
-    uint8_t mode     : 2;
-    uint8_t com      : 1;
-  };
-  uint8_t value;
-}ap_t;
+#define LED_R_PIN           GET_PIN(B, 7)
+#define LED_B_PIN           GET_PIN(B, 3)
+#define K1                  GET_PIN(A, 8)
+#define K2                  GET_PIN(B, 10)
+#define K3                  GET_PIN(B, 11)
+#define K4                  GET_PIN(C, 15)
 
 typedef struct vel_target {
   float vel_x; // m/s
